fca9da078db9a5aebc53d167db8e2093aa060b8f
Vladusick <samuelfs@mail.ru>
2022-11-28 17:19:01 +0400

    task rendering

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
 // Глобальные переменые
+const todoList = document.getElementById('todo-list');
 let todos = [];
 let users = [];
 
 // Attach Events
+// Повесил событиt срабатывания скрипта после загрузки страницы
 document.addEventListener('DOMContentLoaded', initApp);
 
-initApp();
+// Basic logic
+//Функция получения имени по идентификатору
+function getUserName(userId) {
+    const user = users.find(u => u.id === userId);
+    return user.name;
+}
+
+// Функция отрисовки списка задач
+function printTodo({id, userId, title, completed}) {
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+    li.dataset.id = id;
+    li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(userId)}</b></span>`;
+
+    const status = document.createElement('input');
+    status.type = 'checkbox';
+    status.checked = completed;
+
+    const close = document.createElement('span');
+    close.innerHTML = '&times;';
+    close.className = 'close';
+
+    li.prepend(status);
+    li.append(close);
+    todoList.prepend(li);
+}
 
 // Event logic (Функция получение данных и записи)
 function initApp() {
     Promise.all([getAllTodos(), getAllUsers()]).then(values => {
         [todos, users] = values;
-    });
 
-    
+        // Отправка в разметку
+        todos.forEach((todo) => printTodo(todo));
+    })
 }
 
 // Async logic
